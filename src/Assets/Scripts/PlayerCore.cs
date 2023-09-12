96ba00a72852e489fc646969133cb25cd8209e0d
MettoNao <nt7.00.harry@gmail.com>
2023-09-12 13:16:27 +0900

    first commit

---
+++ src/Assets/Scripts/PlayerCore.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCore
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public PlayerCore()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+}
