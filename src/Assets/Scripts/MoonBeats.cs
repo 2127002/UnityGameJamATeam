c146cc516b09aae18cfe7f903396bd24f7661f26
2127002 <104291548+2127002@users.noreply.github.com>
2023-09-12 14:11:23 +0900

    鼓動する月を追加しました
    画像は仮のものなので背景と一緒にデザイナーが作り次第、差し替えてください

---
+++ src/Assets/Scripts/MoonBeats.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonBeats : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool IsBpmAction;
+
+    [SerializeField]
+    private int bpm;
+
+    private Sprite moon;
+    private float time = 0;
+
+    private Animator animator;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (IsBpmAction)
+        {
+            time += Time.deltaTime;
+            if (time >= 60f / bpm)
+            {
+                time -= 60f / bpm;
+                animator.CrossFade("Beat", 0f);
+            }
+        }
+    }
+
+    public void BeatsStart() {
+        IsBpmAction = true;
+        time = 0;
+    }
+
+    public void BeatsEnd()
+    {
+        IsBpmAction = false;
+    }
+}
